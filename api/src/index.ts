567b4788299b2ed419edc0cc499e1da8f5397c38
Donald Pursley <drpursley@gmail.com>
2022-04-03 13:41:59 -0700

    serve static content (images, template json files)

--- api/src/index.ts
+++ api/src/index.ts
@@ -3,6 +3,9 @@ import express from "express";
 const app = express();
 const port = 8080; // default port to listen
 
+// mount static content
+app.use(express.static('public'));
+
 // start the Express server
 app.listen(port, () => {
   console.log(`server started at http://localhost:${ port }`);
