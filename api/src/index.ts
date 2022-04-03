b9827f9854eaefde700fd2c4440ac8c4161efbbf
Donald Pursley <drpursley@gmail.com>
2022-04-03 11:13:52 -0700

    create initial api application
    - stub 'hello world' route in index.ts
    - include typescript and express and node types for dev dependencies

---
+++ api/src/index.ts
@@ -0,0 +1,9 @@
+import express from "express";
+
+const app = express();
+const port = 8080; // default port to listen
+
+// start the Express server
+app.listen(port, () => {
+  console.log(`server started at http://localhost:${ port }`);
+});
